1fd76f1d67171bc2596f24a466e44e111f44f9bd
shen <goodstudyqaq@gmail.com>
2026-08-04 14:22:16 +0200

    archive

--- perturbations/local.sh
+++ perturbations/local.sh
@@ -1,5 +1,7 @@
-export CLEAN_AFTER ="0"
+export QDYN="$HOME/code/Q2/bin/qdyn"
+export QFEP="$HOME/code/Q2/src/q6/bin/q6/qfep"
+export CLEAN_AFTER="0"
 export QGPU_MODULES=""
 ./run_qgpu_mps_local.sh \
---only cdk2/2.protein/FEP_1h1s_31 \
+--only thrombin/2.protein/FEP_1a_1c \
 --replicates 1
